18502671d7f611c96d13158e5a3d84d2cff61268
HominSu <hominsu@foxmail.com>
2023-03-10 21:13:37 +0800

    feat: add file read/write stream
    Signed-off-by: HominSu <hominsu@foxmail.com>

---
+++ include/bencode/file_read_stream.h
@@ -0,0 +1,128 @@
+// MIT License
+//
+// Copyright (c) 2023 HominSu
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//
+// Created by Homin Su on 2023/3/9.
+//
+
+#ifndef BENCODE_INCLUDE_BENCODE_FILE_READ_STREAM_H_
+#define BENCODE_INCLUDE_BENCODE_FILE_READ_STREAM_H_
+
+#include <cstdio>
+
+#include <vector>
+
+#include "bencode.h"
+#include "non_copyable.h"
+
+namespace bencode {
+
+class FileReadStream : NonCopyable {
+ public:
+  using Ch = char;
+
+ private:
+  static const std::size_t kInnerBufferSize = 256;
+  std::FILE *fp_;
+  Ch inner_buffer_[kInnerBufferSize]{};
+  Ch *buffer_;
+  Ch *current_;
+  Ch *buffer_last_;
+  std::size_t buffer_size_;
+  std::size_t read_count_;
+  std::size_t read_total_;
+  bool eof_;
+
+ public:
+  explicit FileReadStream(std::FILE *fp)
+      : fp_(fp),
+        buffer_(inner_buffer_),
+        current_(inner_buffer_),
+        buffer_last_(nullptr),
+        buffer_size_(kInnerBufferSize),
+        read_count_(0),
+        read_total_(0),
+        eof_(false) {
+    BENCODE_ASSERT(fp_ != nullptr && "file pointer should not be empty");
+    read();
+  }
+
+  explicit FileReadStream(std::FILE *fp, char *buffer, std::size_t buffer_size)
+      : fp_(fp),
+        buffer_(buffer),
+        current_(buffer_),
+        buffer_last_(nullptr),
+        buffer_size_(buffer_size),
+        read_count_(0),
+        read_total_(0),
+        eof_(false) {
+    BENCODE_ASSERT(fp_ != nullptr && "file pointer should not be empty");
+    BENCODE_ASSERT(buffer_size_ >= 4 && "buffer size should be bigger then four");
+    read();
+  }
+
+  [[nodiscard]] bool hasNext() const { return !eof_ || (current_ + 1 - !eof_ <= buffer_last_); }
+
+  Ch peek() { return *current_; }
+
+  Ch next() {
+    Ch ch = *current_;
+    read();
+    return ch;
+  }
+
+  template<typename Tint, class = typename std::enable_if_t<std::is_integral_v<std::remove_reference_t<Tint>>>>
+  void next(Tint n) {
+    BENCODE_ASSERT(n >= 0);
+    for (Tint i = 0; i < n; ++i) {
+      if (hasNext()) { read(); }
+      else { break; }
+    }
+  }
+
+  void assertNext(Ch ch) {
+    (void) ch;
+    BENCODE_ASSERT(peek() == ch);
+    read();
+  }
+
+ private:
+  void read() {
+    if (current_ < buffer_last_) { ++current_; }
+    else if (!eof_) {
+      read_total_ += read_count_;
+      read_count_ = std::fread(buffer_, 1, buffer_size_, fp_);
+      buffer_last_ = buffer_ + read_count_ - 1;
+      current_ = buffer_;
+
+      if (read_count_ < buffer_size_) {
+        buffer_[read_count_] = '\0';
+        ++buffer_last_;
+        eof_ = true;
+      }
+    }
+  }
+};
+
+} // namespace bencode
+
+#endif //BENCODE_INCLUDE_BENCODE_FILE_READ_STREAM_H_
---
+++ include/bencode/file_write_stream.h
@@ -0,0 +1,128 @@
+// MIT License
+//
+// Copyright (c) 2023 HominSu
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//
+// Created by Homin Su on 2023/3/10.
+//
+
+#ifndef BENCODE_INCLUDE_BENCODE_FILE_WRITE_STREAM_H_
+#define BENCODE_INCLUDE_BENCODE_FILE_WRITE_STREAM_H_
+
+#include <cstdio>
+#include <cstring>
+
+#include <string_view>
+
+#include "bencode.h"
+#include "non_copyable.h"
+
+namespace bencode {
+
+class FileWriteStream : NonCopyable {
+ private:
+  std::FILE *out_;
+  char *buffer_;
+  char *buffer_end_;
+  char *current_;
+
+ public:
+  explicit FileWriteStream(std::FILE *out, char *buffer, std::size_t buffer_size)
+      : out_(out),
+        buffer_(buffer),
+        buffer_end_(buffer + buffer_size),
+        current_(buffer_) {
+    BENCODE_ASSERT(out != nullptr && "FILE pointer equal zero");
+  }
+  ~FileWriteStream() { if (current_ != buffer_) { flush(); }}
+
+  void put(char ch) {
+    if (current_ >= buffer_end_) { flush(); }
+    *current_++ = ch;
+  }
+
+  void put_n(char ch, std::size_t size) {
+    auto avail = static_cast<std::size_t>(buffer_end_ - current_);
+    while (size > avail) {
+      std::memset(current_, ch, avail);
+      current_ += avail;
+      flush();
+      size -= avail;
+      avail = static_cast<size_t>(buffer_end_ - current_);
+    }
+    if (size > 0) {
+      std::memset(current_, ch, size);
+      current_ += size;
+    }
+  }
+
+  void puts(const char *str, std::size_t length) {
+    auto avail = static_cast<std::size_t>(buffer_end_ - current_);
+    std::size_t copy = 0;
+
+    while (length > avail) {
+      std::memcpy(current_, str + copy, avail);
+      current_ += avail;
+      copy += avail;
+      flush();
+      length -= avail;
+      avail = static_cast<std::size_t>(buffer_end_ - current_);
+    }
+
+    if (length > 0) {
+      std::memcpy(current_, str + copy, length);
+      current_ += length;
+    }
+  }
+
+  void put_sv(std::string_view sv) {
+    auto avail = static_cast<std::size_t>(buffer_end_ - current_);
+    std::size_t length = sv.length();
+    std::size_t copy = 0;
+
+    while (length > avail) {
+      std::memcpy(current_, sv.data() + copy, avail);
+      current_ += avail;
+      copy += avail;
+      flush();
+      length -= avail;
+      avail = static_cast<size_t>(buffer_end_ - current_);
+    }
+
+    if (length > 0) {
+      std::memcpy(current_, sv.data() + copy, length);
+      current_ += length;
+    }
+  }
+
+  void flush() {
+    if (current_ != buffer_) {
+      size_t result = std::fwrite(buffer_, 1, static_cast<size_t>(current_ - buffer_), out_);
+      if (result < static_cast<size_t>(current_ - buffer_)) {
+      }
+      current_ = buffer_;
+    }
+  }
+};
+
+} // namespace bencode
+
+#endif //BENCODE_INCLUDE_BENCODE_FILE_WRITE_STREAM_H_
